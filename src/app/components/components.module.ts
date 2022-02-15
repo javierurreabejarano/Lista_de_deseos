68dad51fa3bac7b9c02efd0ae4391bcdabc09dcf
javierurreabejarano <javier.technologies@gmail.com>
2022-02-15 10:58:25 -0500

    Aplicación en IONIC

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListasComponent } from './listas/listas.component';
+import { IonicModule } from '@ionic/angular';
+import { DeseosService } from '../services/deseos.service';
+import { PipesModule } from '../pipes/pipes.module';
+
+
+
+@NgModule({
+  declarations: [
+    ListasComponent
+  ],
+  exports: [
+    ListasComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule,
+    PipesModule
+  ]
+})
+export class ComponentsModule { }
